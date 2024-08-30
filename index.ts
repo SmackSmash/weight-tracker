aca91fb0ee47a4d624abd82b455af33ad99e3769
Dan Moffat <danmoffat@gmail.com>
2024-08-30 15:01:52 +0100

    CORS and json setup

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
-import express, { type Request, type Response } from 'express';
-const app = express();
+import express, { type Express, type Request, type Response } from 'express';
+import cors from 'cors';
+const app: Express = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello world');
