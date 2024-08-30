db0acfb769d5c423540a216d29c4f272b7bf7ca1
Dan Moffat <danmoffat@gmail.com>
2024-08-30 15:59:23 +0100

    Added basic line chart with chart.js

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,76 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  responsive: true,
+  interaction: {
+    mode: 'index' as const,
+    intersect: false
+  },
+  stacked: false,
+  plugins: {
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart - Multi Axis'
+    }
+  },
+  scales: {
+    y: {
+      type: 'linear' as const,
+      display: true,
+      position: 'left' as const
+    },
+    y1: {
+      type: 'linear' as const,
+      display: true,
+      position: 'right' as const,
+      grid: {
+        drawOnChartArea: false
+      }
+    }
+  }
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: [1, 2, 3, 4, 5, 6, 7],
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      yAxisID: 'y'
+    }
+  ]
+};
+
 const App = () => {
-  return <div>App works!</div>;
+  return (
+    <div>
+      <Line options={options} data={data} />
+    </div>
+  );
 };
 
 export default App;
