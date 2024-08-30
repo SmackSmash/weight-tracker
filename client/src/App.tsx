08cdf91da810841662d573cb7e4d083836e55b0e
Dan Moffat <danmoffat@gmail.com>
2024-08-30 16:03:39 +0100

    A few tweaks

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -30,7 +30,8 @@ const options = {
   plugins: {
     title: {
       display: true,
-      text: 'Chart.js Line Chart - Multi Axis'
+      color: 'white',
+      text: 'Weight Tracker'
     }
   },
   scales: {
@@ -38,14 +39,6 @@ const options = {
       type: 'linear' as const,
       display: true,
       position: 'left' as const
-    },
-    y1: {
-      type: 'linear' as const,
-      display: true,
-      position: 'right' as const,
-      grid: {
-        drawOnChartArea: false
-      }
     }
   }
 };
